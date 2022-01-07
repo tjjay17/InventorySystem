09a034833a7b47b1e71fac0a676c8a6a163b6bdf
tjjay17 <56407501+tjjay17@users.noreply.github.com>
2022-01-06 23:34:45 -0500

    added route skeleton

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const routes = require('./routes');
 const app = express();
 
+app.use('/', routes);
 app.listen(3000);
